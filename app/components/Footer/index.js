99af382ed989db8fd43cb985db3450f41c3d40f7
Luandro <luandro@gmail.com>
2016-09-18 16:07:04 -0300

    Footer component

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -5,16 +5,19 @@
 */
 
 import React from 'react';
-
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-
+import Marker from './marker.svg';
 import styles from './styles.css';
 
 function Footer() {
   return (
-    <div className={styles.footer}>
-      <FormattedMessage {...messages.header} />
+    <div className={styles.wrapper}>
+      <div className={styles.location}>
+        <img src={Marker} alt="local" />
+        <span>em Alter do Chão, Pará</span>
+      </div>
+      <div className={styles.mail}>
+        <a target="_blank" mailto="luandro@gmail.com">luandro@gmail.com</a>
+      </div>
     </div>
   );
 }
